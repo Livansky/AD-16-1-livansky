de6be6c37d8351657e2392b7e7c7396a7265b2cd
Carlos Ortiz <carlos72ortiz14@gmail.com>
2025-09-25 15:40:05 -0600

    se hizo task1

---
+++ task1.js
@@ -0,0 +1,6 @@
+function js_style() {
+  const paragraph = document.getElementById("text");
+  paragraph.style.fontFamily = "Arial, sans-serif";
+  paragraph.style.fontSize = "36px";
+  paragraph.style.color = "green";
+}
